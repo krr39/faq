8a2aaffddf96306973e45f69bdd11f9c7a50aadc
krutika <krr39@njit.edu>
2018-12-12 01:04:35 -0500

    resolved the error null ref integrity due to null value in tag (facing this issue while applying migrate refresh command while creating model for calc)

--- database/migrations/2018_12_12_053149_create_calcs_table.php
+++ database/migrations/2018_12_12_053149_create_calcs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQuestionsTable extends Migration
+class CreateCalcsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateQuestionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('questions', function (Blueprint $table) {
+        Schema::create('calcs', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->longText('body');
-            $table->longText('tag')->nullable();
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateQuestionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('calcs');
     }
 }
