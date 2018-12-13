de037461248ff22094e143fdfae8d950631f4a38
krutika <krr39@njit.edu>
2018-12-12 22:56:19 -0500

    add code to generate fake value

--- database/factories/CalculatorFactory.php
+++ database/factories/CalculatorFactory.php
@@ -5,5 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(Model::class, function (Faker $faker) {
     return [
         //
+        'question_weightage' => $faker->randomNumber(2),
+        'no_of_questions' => $faker->randomNumber(2),
+        'total' => $faker->randomNumber(2),
     ];
 });
