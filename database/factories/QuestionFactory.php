82182231b803d9b5d1cb117a136d7f8fc93d131e
krutika <krr39@njit.edu>
2018-12-14 22:23:20 -0500

    added faker code in factory to generate random name in tag

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -6,6 +6,7 @@ $factory->define(App\Question::class, function (Faker $faker) {
     return [
         //
         'body' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+        'tag' => $faker->randomElement($array = array ('#facebook','#what','#how','#Quara','#AskMe')),
 
 
     ];
