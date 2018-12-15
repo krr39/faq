ec8288934941fbdb4d598ddf4116bd7d31bb2f2e
krutika <krr39@njit.edu>
2018-12-14 23:20:40 -0500

    finish tag functionality

--- resources/views/questionForm.blade.php
+++ resources/views/questionForm.blade.php
@@ -20,6 +20,7 @@
                             <div class="form-group">
                                 {!! Form::label('tag', 'Tag') !!}
                                 {!! Form::text('tag', $question->body, ['class' => 'form-control','required' => 'required']) !!}
+                                <p>Add # before adding tag and space between tag keywords</p>
                             </div>
                         <button class="btn btn-success float-right" value="submit" type="submit" id="submit">Save
                         </button>
