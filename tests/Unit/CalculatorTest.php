8c27001ec813762ac38c746c2718c41c955b898f
krutika <krr39@njit.edu>
2018-12-15 20:07:57 -0500

    test the calculator form for no of question is interger

--- tests/Unit/CalculatorTest.php
+++ tests/Unit/CalculatorTest.php
@@ -23,11 +23,20 @@ class CalculatorTest extends TestCase
         $calc = factory(\App\Calc::class)->make();
         $this->assertTrue($calc->save());
     }
-    public function testCalculatorFieldIsInteger()
+    //Unit test to check the question weightage field is integer
+    public function testCalculatorWeightageFieldIsInteger()
     {
         $calc = Calc::inRandomOrder()->first();
         $question_weightage = (int)$calc->question_weightage;
         $this->assertInternalType("int", $question_weightage);
     }
+    //Unit test to check the no of question field is integer
+    public function testNoOfQuestionFieldIntefer()
+    {
+        $calc = Calc::inRandomOrder()->first();
+        $no_of_questions = (int)$calc->no_of_questions;
+        $this->assertInternalType("int", $no_of_questions);
+    }
+
 
 }
