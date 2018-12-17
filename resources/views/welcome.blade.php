f8e410b671558d65833d4c7cda3f7c6dc26b5296
krutika <krr39@njit.edu>
2018-12-17 00:06:56 -0500

    change the slide image code of home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,164 +1,155 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
-        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
-        <script type="text/javascript" src="https://code.jquery.com/jquery.min.js"></script>
-        <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-        <style>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-            h2{
-                margin: 0;
-                color: #666;
-                padding-top: 90px;
-                font-size: 52px;
-                font-family: "trebuchet ms", sans-serif;
-            }
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 
-            .item{
-                background: white;
-                text-align: center;
-                height: 450px !important;
-                width: 800px ;
-            }
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+        .full-height {
+            height: 100vh;
+        }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+        .position-ref {
+            position: relative;
+        }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+        .content {
+            text-align: center;
+        }
+        .title {
+            font-size: 84px;
+        }
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
 
-            .carousel{
-                margin-top: 20px;
-            }
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
 
-            .bs-example{
-                margin: 20px;
-            }
+    <div class="content">
+        <div id="demo" class="carousel slide" data-ride="carousel">
 
-            .carousel.carousel-fade .item {
-                opacity:0;
-                filter: alpha(opacity=0); /* ie fix */
-            }
+            <!-- Indicators -->
+            <ul class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+            </ul>
 
-            .carousel.carousel-fade .active.item {
-                opacity:1;
-                filter: alpha(opacity=100); /* ie fix */
-            }
-
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div id="myCarousel" class="carousel slide" data-interval="6500" data-ride="carousel">
-                    <!-- Carousel indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="" data-slide-to="0" class="active"></li>
-                        <li data-target="#myCarousel" data-slide-to="1"></li>
-                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                    </ol>
-                    <!-- Carousel items -->
-                    <div class="carousel-inner">
-                        <div class="active item carousel-fade">
-                            <h2>Slide 1</h2>
-                            <div class="carousel-caption">
-                                <img class="img-responsive" height="450" width="850" src="https://images.unsplash.com/photo-1419064642531-e575728395f2?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=76d5c793e4f8d02d7a9be27bc71256f7" data-color="violet" alt="Third Image">
-                                <h3>First slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </img>
-                            </div>
-                        </div>
-                        <div class="item carousel-fade">
-                            <h2>Slide 2</h2>
-                            <div class="carousel-caption">
-                                <h3>Second slide label</h3>
-                                <p>Aliquam sit amet gravida nibh, facilisis gravida odio.</p>
-                            </div>
-                        </div>
-                        <div class="item carousel-fade">
-                            <h2>Slide 3</h2>
-                            <div class="carousel-caption">
-                                <h3>Third slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                            </div>
-                        </div>
+            <!-- The slideshow -->
+            <div class="carousel-inner well-lg" >
+                <div class="carousel-item active">
+                    <img src="http://www.top13.net/wp-content/uploads/2012/01/Bernstorff-Isofjord-Greenland-1024x768.jpg" alt="Nature2">
+                    <div class="carousel-caption">
+                        <p>What is the name of the place?</p>
+                        <h3> Bernstorff Isofjord, Greenland</h3>
                     </div>
-                    <!-- Carousel nav -->
-                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                    </a>
-                    <a class="carousel-control right" href="#myCarousel" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                    </a>
+
+                </div>
+                <div class="carousel-item">
+                    <img src="http://www.top13.net/wp-content/uploads/2012/01/Madeira-Island-Portugal-1024x768.jpg" alt="Nature1">
+                    <div class="carousel-caption">
+                        <p>What is the name of the place?</p>
+                        <h3> Madeira Island, Portugal</h3>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="http://www.top13.net/wp-content/uploads/2012/01/nepal.jpg" alt="Nature3" alt="Nature3">
+                    <div class="carousel-caption">
+                        <h3>Khangsar, Nepal</h3>
+                        <p>Would you like to explore this place?</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="http://www.top13.net/wp-content/uploads/2012/01/Ban-Gioc-Waterfall-1024x768.jpg" alt="Nature4">
+                    <div class="carousel-caption">
+                        <h3>Ban Gioc Waterfall, Cao Bang Province, Vietnam</h3>
+                        <p>Would you like to visit this place?</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="http://www.top13.net/wp-content/uploads/2012/01/The-Wave-at-the-Vermillion-Cliffs-National-Monument-ArizonaUtah-1024x768.jpg" alt="Nature5">
+                    <div class="carousel-caption">
+                        <p>Omg..!! what a beautiful cliffs it is?</p>
+                        <h3>The Wave at the Vermillion Cliffs National Monument (Arizona, Utah)
+                        </h3>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="http://www.top13.net/wp-content/uploads/2012/01/Columbia-River-Gorge-Oregon-1024x768.jpg" alt="Nature6">
+                    <div class="carousel-caption">
+                        <p>So, what could be name of wonderfull river?</p>
+                        <h3>Columbia River Gorge, Oregon</h3>
+                    </div>
                 </div>
             </div>
-        </div>
-    </body>
+
+            <!-- Left and right controls -->
+            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#demo" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
+
+
+    </div>
+</div>
+    </div>
+</body>
 </html>
